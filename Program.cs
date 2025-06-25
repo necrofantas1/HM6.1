0e85d1349264dc1304dedb2a12f30d46ec45732f
Vladyslav <vladchershev@gmail.com>
2025-06-25 16:24:14 +0200

    adding project files

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿#region Обчислення середнього заробітку:
+bool isValidInput = true;
+
+const char cancelChar = 'x';
+decimal totalSalary = 0;
+int workers = 0;
+decimal averageSalary; 
+
+Console.WriteLine("Введіть заробітки працівників (введіть 'x' для виходу):");
+
+while (isValidInput)
+{
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == cancelChar.ToString())
+    {
+        isValidInput = false;
+        continue;
+    }
+
+    if (int.TryParse(input, out int salary) && salary >= 0)
+    {
+        totalSalary += salary;
+        workers++;
+    }
+    else
+    {
+        Console.WriteLine("Невірний ввід. Будь ласка, введіть невід'ємне число або 'x' для виходу.");
+    }
+
+    // вивід середнього заробітку після кожного введення
+    if (workers > 0)
+    {
+       averageSalary = totalSalary / workers;
+       Console.WriteLine($"Середній заробіток працівників: {averageSalary}");
+    }
+    
+}
+#endregion
+
